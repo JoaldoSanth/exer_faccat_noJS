8599373dadec41b0f9c68977907c711c43a73c96
JoaldoSanth <joaldosant3@gmail.com>
2024-04-22 00:47:03 -0300

    exercicios_faccat_no_JS

---
+++ Exercício_13/script.js
@@ -0,0 +1,10 @@
+let nota1;
+let nota2;
+let nota3;
+let total_media;
+nota1 = parseInt(prompt("Digite a primeira nota do aluno"));
+nota2 = parseInt(prompt("Digite a primeira nota do aluno"));
+nota3 = parseInt(prompt("Digite a primeira nota do aluno"));
+
+total_media = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10;
+alert("A media final do aluno é: " + total_media);
